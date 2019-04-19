99b99aa6404b73676228ecf34899466d07bf3fff
ssilentsharer <ssilentsharer@gmail.com>
2019-04-19 19:35:54 +0800

    supported file lock

--- file.go
+++ file.go
@@ -1,11 +1,19 @@
 package ioengine
 
-import "os"
+import (
+	"errors"
+	"os"
+	"sync"
+)
+
+var once sync.Once
 
 // FileIO Standrad I/O mode
 type FileIO struct {
-	opt Options
+	path string
+	opt  Options
 	*os.File
+	*FileLock
 }
 
 func newFileIO(name string, opt Options) (*FileIO, error) {
@@ -13,5 +21,60 @@ func newFileIO(name string, opt Options) (*FileIO, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &FileIO{File: fd, opt: opt}, nil
+
+	fi := &FileIO{path: name, opt: opt, File: fd}
+
+	switch opt.FileLock {
+	case None:
+	case ReadWrite:
+		fi.FileLock, err = NewFileLock(name, true)
+		if err != nil {
+			fd.Close()
+			return nil, err
+		}
+	case ReadOnly:
+		fi.FileLock, err = NewFileLock(name, false)
+		if err != nil {
+			fd.Close()
+			return nil, err
+		}
+	}
+
+	return fi, nil
+}
+
+// FLock a file lock is a recommended lock.
+// if file lock not init, we will init once.
+func (fi *FileIO) FLock() (err error) {
+	if fi.FileLock == nil {
+		once.Do(func() {
+			if fi.FileLock == nil {
+				fi.FileLock, err = NewFileLock(fi.path, true)
+			}
+		})
+	}
+	if err != nil {
+		return err
+	}
+	if fi.FileLock == nil {
+		return errors.New("Uninitialized lock")
+	}
+	return fi.FileLock.FLock()
+}
+
+// FUnlock file unlock
+func (fi *FileIO) FUnlock() error {
+	if fi.FileLock == nil {
+		return nil
+	}
+	return fi.FileLock.FUnlock()
+}
+
+// Close impl standard File Close method
+func (fi *FileIO) Close() error {
+	if fi.FileLock == nil {
+		return fi.File.Close()
+	}
+	fi.FileLock.Release()
+	return fi.File.Close()
 }
