2aa0e1b1c4f8bb3fad993609a8d9ce5c4518f04b
ssilentsharer <ssilentsharer@gmail.com>
2019-04-19 19:33:45 +0800

    wraps generic pwritev and append

--- syscall.go
+++ syscall.go
@@ -1,4 +1,51 @@
 package ioengine
 
+import (
+	"io"
+	"os"
+	"syscall"
+)
+
 // Single-word zero for use when we need a valid pointer to 0 bytes.
 var zero uintptr
+
+func genericWriteAtv(fd File, bs [][]byte, off int64) (n int, err error) {
+	nOffset := off
+	nw := 0
+
+	for _, b := range bs {
+		nw, err = fd.WriteAt(b, nOffset)
+		n += nw
+		nOffset += int64(nw)
+		if err != nil {
+			if err.(syscall.Errno) == syscall.EAGAIN {
+				continue
+			}
+			break
+		}
+		if nw == 0 {
+			err = io.ErrUnexpectedEOF
+			break
+		}
+	}
+
+	return n, err
+}
+
+func genericAppend(fd File, bs [][]byte) (int, error) {
+	stat, err := fd.Stat()
+	if err != nil {
+		return 0, err
+	}
+
+	// open file with O_APPEND not need to flock
+	if stat.Mode & os.O_APPEND {
+		return fd.WriteAtv(bs, stat.Size())
+	}
+
+	if err := fd.FLock(); err != nil {
+		return err
+	}
+	err = fd.WriteAtv(bs, stat.Size())
+	return fd.FUnlock()
+}
